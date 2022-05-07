dcd4ac9249b5ae5f9e08245a52d4ea167a4e8b04
rattledoge <71441854+rattledoge@users.noreply.github.com>
2022-05-07 12:24:10 +0300

    todo add route is added

--- API/source/todos.py
+++ API/source/todos.py
@@ -1,11 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, current_app
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 todos=Blueprint("todos", __name__, url_prefix='/api/v1/todos')
 
 @todos.route('/add', methods=['POST'])
+@jwt_required()
 def add():
-    return 'add todo'
+    user_obj_id = get_jwt_identity()
+    header = request.json['header']
+    detail = request.json['detail']
+    todo=jsonify({
+        'header': header,
+        'detail': detail,
+        'iscompleted': False,
+        'userid': user_obj_id
+    })
+    current_app.db.todos.insert_one({
+        'header': header,
+        'detail': detail,
+        'iscompleted': False,
+        'userid': user_obj_id
+    })
+    return jsonify({'message': "Todo is added"}), 200
 
 @todos.route('/update/<oid>', methods=['PUT'])
 def update():
